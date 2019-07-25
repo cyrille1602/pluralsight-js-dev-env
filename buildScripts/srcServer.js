f33c271fceec6952a0b09642c43ac96941909424
cyrille1602 <c.ngnechie@gmail.com>
2019-07-25 21:51:55 +0200

    New progress

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import path  from 'path';
-import open from'open';
+import path from 'path';
+import open from 'open';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
-/*eslint-disable no-console */
+/* eslint-disable no-console */
 
 const port = 3000;
 const app = express();
@@ -12,11 +12,20 @@ const compiler = webpack(config);
 
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
-  publicPath: config.output.publicPath,
+  publicPath: config.output.publicPath
 }));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.get('/users', function(req, res) {
+  // Hard coding for simplicity. Pretend this hits a real database
+  res.json([
+    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
+    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
+  ]);
 });
 
 app.listen(port, function(err) {
--- src/index.test.js
+++ src/index.test.js
@@ -4,18 +4,18 @@ import fs from 'fs';
 
 describe('Our first test', () => {
   it('should pass', () => {
-      expect(true).to.equal(true);
+    expect(true).to.equal(true);
   });
 });
 
 describe('index.html', () => {
-  it('should say hello', (done) =>{
+  it('should have h1 that says Users', (done) => {
     const index = fs.readFileSync('./src/index.html', "utf-8");
     jsdom.env(index, function(err, window) {
       const h1 = window.document.getElementsByTagName('h1')[0];
-      expect(h1.innerHTML).to.equal("Hello World!");
+      expect(h1.innerHTML).to.equal("Users");
       done();
       window.close();
     });
-  });
-});
+  })
+})
